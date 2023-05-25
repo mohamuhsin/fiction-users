deac77dfce198b90bcbe3b557fcc7d14659aff5b
mohamuhsin <muhsinohoho@outlook.com>
2023-05-25 20:49:48 +0300

    AddUser Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,8 +2,10 @@ import React from 'react';
 const AddUser = (props) => {
 return (
     <form>
-        <lable htmlFor='username'>Username</lable>
+        <label htmlFor='username'>Username</label>
         <input id='username' type='text' />
+        <label htmlFor='age'>Age (Years)</label>
+        <button type='submit'>Add User</button>
     </form>
 )
 }
