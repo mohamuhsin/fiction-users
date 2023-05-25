bed5b7f1b223a9dbb6adc4d7ff0f2fdaecd4f532
mohamuhsin <muhsinohoho@outlook.com>
2023-05-25 22:15:23 +0300

    styling the button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,6 +2,6 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
-return <button className={classes.button} type={props.type || 'button'}></button>
+return <button className={classes.button} type={props.type || 'button'} onClick={props.onClick}></button>
 }
 export default Button;
