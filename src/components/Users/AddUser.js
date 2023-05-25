69ed8fec55b574e647271d0bfd5c47940ff94ce1
mohamuhsin <muhsinohoho@outlook.com>
2023-05-25 20:47:58 +0300

    AddUser Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,10 @@
 import React from 'react';
-const AddUser = () => {
-
+const AddUser = (props) => {
+return (
+    <form>
+        <lable htmlFor='username'>Username</lable>
+        <input id='username' type='text' />
+    </form>
+)
 }
 export default AddUser;
