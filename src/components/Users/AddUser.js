f4906f5a00d2414994e961b015ec5ec26b18e3f5
mohamuhsin <muhsinohoho@outlook.com>
2023-05-26 21:47:04 +0300

    added state changes

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -11,12 +11,20 @@ const AddUser = (props) => {
         event.preventDefault();
 }
 
+const usernameChangeHandler = (event) => {
+setenteredUsername(event.target.value);
+}
+
+const ageChangeHandler = (event) => {
+    setenteredUsername(event.target.value);
+    }
+
 return (
     <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
+        <label htmlFor='username' onChange={usernameChangeHandler}>Username</label>
         <input id='username' type='text' />
-        <label htmlFor='age'>Age (Years)</label>
+        <label htmlFor='age' onChange={ageChangeHandler}>Age (Years)</label>
         <input id='age' type='number' />
         <Button type='submit'>Add User</Button>
     </form>
