847846cc9f24df346ef9dc4a20af9e76c365280a
mohamuhsin <muhsinohoho@outlook.com>
2023-05-26 22:15:58 +0300

    created UsersList component

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const UsersList = (props) => {
-    return(<div></div>);
+    return(<ul>
+        {props.users.map(user => 
+            <li>{user.name} ({user.age } years old)</li>)}
+    </ul>);
 }
 export default UsersList;
